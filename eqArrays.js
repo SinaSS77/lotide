e1f8d4f297fe0241f044708083dbbc6a691e88a2
SinaSS77 <s.molaverdikhani>
2023-01-10 21:25:48 -0800

    Using assertEqual to compare two arrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("PASS");
+  } else {
+    console.log("Fail");
+  }
+};
+
+const eqArrays = (array1, array2) => {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [4, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [4, 2, 3]), false);
