97cb95df7195fe01e16e04198c4748caf3f3dd9b
SinaSS77 <s.molaverdikhani>
2023-01-11 16:19:07 -0800

    modified: will compare to arrays and return a subArray

--- without.js
+++ without.js
@@ -21,11 +21,11 @@ const assertArraysEqual = (array1, array2) => {
 const without = (array1, array2) => {
   let resultArray = [];
   for (let i = 0; i < array1.length; i++) {
-    if (!array2.includes(array1[i])){
-      resultArray.push(array1[i])
+    if (!array2.includes(array1[i])) {
+      resultArray.push(array1[i]);
     }
   }
   return resultArray;
 };
 
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]),["1", "2"])
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
