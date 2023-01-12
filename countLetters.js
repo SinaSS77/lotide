ac5bc2282ec32c3b4804622e5ae3de26181d5f30
SinaSS77 <s.molaverdikhani>
2023-01-11 16:14:05 -0800

    it will count the letters in a string

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (anyString) {
+  let arrayString = anyString.split("");
+  let resultObject = {};
+  for (let letter of anyString) {
+    if (!resultObject.hasOwnProperty(letter)) {
+      let container = 0;
+      arrayString.forEach((element) => {
+        if (element === letter) {
+          container++;
+        }
+      });
+      resultObject[letter] = container;
+    }
+  }
+  console.log(resultObject);
+};
+
+countLetters("sinasin");
