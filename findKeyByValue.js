7ed7bdf7acdbf92a432ea9e5c2c2dd3cde4761a4
SinaSS77 <s.molaverdikhani>
2023-01-11 17:16:40 -0800

    it will find a key in an object by iterating through values

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (anyObject, anyValue) {
+  let result;
+  for (let item in anyObject) {
+    if (anyValue === anyObject[item]) {
+      result = item;
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
