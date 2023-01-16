ca9ab5d15072f6914747bb8879b0ce22a804bd9a
SinaSS77 <s.molaverdikhani>
2023-01-16 14:30:01 -0800

    for getting an Array include different datas and output a flat Array

--- flatten.js
+++ flatten.js
@@ -1,19 +1,3 @@
-const eqArrays = (array1, array2) => {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = (array1 , array2) => {
-  (eqArrays(array1,array2)) ? console.log("Pass") : console.log("Fail");
-};
 
 const flatten = (array) => {
   let result = [];
@@ -27,4 +11,6 @@ const flatten = (array) => {
   console.log(result);
 };
 
+
+module.exports = flatten;
 
