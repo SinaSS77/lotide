6ed4fca30f1ff3317849d563c8554f9e018867ea
SinaSS77 <s.molaverdikhani>
2023-01-16 09:52:56 -0800

    array head test with mocha $ chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,27 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+// const assertEqual = require('../assertEqual');
+const head = require("../head");
+const assert = require("chai").assert;
 
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), 5);
+describe("return the very first element of any array file", function () {
+  let array1 = [5, 6, 7];
+  let array2 = ["Hello", "Lighthouse", "Labs"];
+  let array3 = [];
+  it(`should return " 5 " for this `, () => {
+    assert.strictEqual(head(array1), 5);
+  });
+  
+  it(`should return " 3 " for this `, () => {
+    assert.strictEqual(head([3]), 3);
+  });
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+  it(`should return "Hello" for this`, () => {
+    assert.strictEqual(head(array2), "Hello");
+  });
+
+  it(`should return "undefined" for this`, () => {
+    assert.strictEqual(head(array3), undefined);
+  });
+});
