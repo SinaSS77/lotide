cd3e6c8d61cc17804e71cbe11e0cd23ad25a7a77
SinaSS77 <s.molaverdikhani>
2023-01-16 09:54:36 -0800

    array tail test with mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,24 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require('chai').assert;
 
 
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe('should return all elements of an array but the very first one', () => {
+  it(`should return ["Lighthouse", "Labs"] for this`, () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it(`should return [2, 3, 4, 5] for this`, () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+});
