9fb45e18e4ac6b3a21cc72d4293aec992827997e
SinaSS77 <s.molaverdikhani>
2023-01-16 14:10:06 -0800

    to compare two objects for equality

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,27 +1,10 @@
-const assertEquals = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEquals = require('./assertEqual');
 
-const eqArrays = (array1, array2) => {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
@@ -40,20 +23,22 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
-// FUNCTION IMPLEMENTATION
-const assertObjectsEqual = function (actual, expected) {
-  const inspect = require('util').inspect;
-  // console.log(`Example label: ${inspect(actual)}`);
-  if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-  }
-};
+module.exports = eqObjects;
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = {
-  size: "medium",
-  colors: ["red", "blue"],
-};
-assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
+// // FUNCTION IMPLEMENTATION
+// const assertObjectsEqual = function (actual, expected) {
+//   const inspect = require('util').inspect;
+//   // console.log(`Example label: ${inspect(actual)}`);
+//   if (eqObjects(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+//   }
+// };
+
+// const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+// const anotherMultiColorShirtObject = {
+//   size: "medium",
+//   colors: ["red", "blue"],
+// };
+// assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
