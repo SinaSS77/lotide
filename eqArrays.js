1ddd7b25f6747a51f1ce61f80ade608805402e84
SinaSS77 <s.molaverdikhani>
2023-01-16 14:24:05 -0800

    To compare the equality of two arrays based on the correctness required in the function

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,15 @@
-const assertEqual = require("./assertEqual")
+const assertEqual = require("./assertEqual");
 
-const eqArrays = (array1, array2) => {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = eqArrays;
-
-
