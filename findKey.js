32a22f003d85cb24a600f4f66aad4540697c087e
SinaSS77 <s.molaverdikhani>
2023-01-16 14:27:18 -0800

    a perfect undrestanding of objects and callback function, return the first key with the wanted value.

--- findKey.js
+++ findKey.js
@@ -1,10 +1,5 @@
-const assertEquals = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+// a function that will be passed an object and a function. so will go to iterate through the object and will return the first key with the wanted value.
 
 const findKey = (obj, callback) => {
   for (let item in obj)
@@ -12,16 +7,18 @@ const findKey = (obj, callback) => {
       return item;
 };
 
-let output = findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
-); // => "noma"
+module.exports = findKey;
 
-assertEquals(output, "noma");
+// let output = findKey(
+//   {
+//     "Blue Hill": { stars: 1 },
+//     Akaleri: { stars: 3 },
+//     noma: { stars: 2 },
+//     elBulli: { stars: 3 },
+//     Ora: { stars: 2 },
+//     Akelarre: { stars: 3 },
+//   },
+//   (x) => x.stars === 2
+// ); // => "noma"
+
+// assertEquals(output, "noma");
