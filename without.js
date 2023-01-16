2d1cbab13477ea81c87a9a7e0716d7028bbbaf9f
SinaSS77 <s.molaverdikhani>
2023-01-16 14:38:50 -0800

    will compare two arrays and return a subArray

--- without.js
+++ without.js
@@ -1,21 +1,3 @@
-// to compare two arrays for equality
-const eqArrays = (array1, array2) => {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
-// to log in console after comparing
-const assertArraysEqual = (array1, array2) => {
-  eqArrays(array1, array2) ? console.log("Pass") : console.log("Fail");
-};
 
 // a function to return a subset of a given array, removing unwanted elements.
 const without = (array1, array2) => {
@@ -28,4 +10,5 @@ const without = (array1, array2) => {
   return resultArray;
 };
 
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+module.exports = without;
+// assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
